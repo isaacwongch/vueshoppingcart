9279228836fa1f49ec15df9f71381e5b709d79cb
isaacwongch <isaac_wch@outlook.com>
2018-03-28 11:44:46 +0800

    check in

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app" class="container my-5">
+    <h1>My online store</h1>
+  </div>
+</template>
+<script>
+import Product from './components/Product';
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      // Nothing for now.
+    };
+  },
+  components: {
+    Product
+  },
+};
+</script>
+<style>
+  /* Nothing for now */
+</style>
---
+++ src/components/Product.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="place">
+        <p>{{ name }}</p>
+        <img :src="img" width="235" height="300"/>
+        <slot></slot>
+        <p>{{ desc }}</p>
+    </div>
+</template>
+<script>
+export default{
+    name: 'product',
+    props: ['name','desc','img'],
+}
+</script>
